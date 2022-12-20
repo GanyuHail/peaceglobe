9f4a7e678a5a006d0023cedf9d90207397c5e4c3
HailGanyu <henryyanguk@gmail.com>
2022-12-20 18:12:23 +0000

    push alt index

--- globe.js
+++ globe.js
@@ -157,7 +157,7 @@ var Globe = function Globe(container, urls) {
             'varying vec3 vNormal;',
             'void main() {',
               'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 7.0 );',
-              'gl_FragColor = vec4( 0.7, 1.0, 0.7, 1.0 ) * intensity;',
+              'gl_FragColor = vec4( 0.7, 1.0, 1.0, 1.0 ) * intensity;',
             '}'
           ].join('\n'),
           side: THREE.BackSide,
